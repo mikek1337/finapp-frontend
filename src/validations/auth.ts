51809e90bb7413c7912f43bcdfc31704da4e7140
mikek1337 <mikekegnlyew@gmail.com>
2026-04-03 10:08:54 +0300

    fix: added validation for user authentication

--- src/validations/auth.ts
+++ src/validations/auth.ts
@@ -1,9 +1,14 @@
+import { error } from "better-auth/api";
 import { z } from "zod";
 
 export const AuthSchema = z.object({
-  name:z.string(),
-  email: z.string(),
-  password: z.string(),
+  name:z.string().min(1,{error: 'name required'}),
+  email: z.email().min(1,{error: 'email required'}),
+  password: z.string().min(8,{error: 'Password requires 8 characters'})
+                      .regex(new RegExp('[A-Z]'),{error:'password requires at least 1 uppercase character'})
+                      .regex(new RegExp('[a-z]'), {error: 'password requires at least 1 lowercase character'})
+                      .regex(new RegExp('[0-9]'), {error: 'password requires at least 1 digit'})
+                      .regex(new RegExp('(?=.*[!@#$%^&*])'), {error: 'password requires a special characters'}),
 });
 
 export const LoginSchema = AuthSchema.omit({
